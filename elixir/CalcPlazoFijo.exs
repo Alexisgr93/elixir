42e6a11cb21f5264cb814da2a58f5422a5882d10
Alexis Rodriguez <76778611+Alexisgr93@users.noreply.github.com>
2021-07-22 20:23:15 -0300

    Add files via upload

--- elixir/CalcPlazoFijo.exs
+++ elixir/CalcPlazoFijo.exs
@@ -1,11 +1,39 @@
+IO.puts("Bienvenido a la calculadora de Plazo Fijo Elixir\n")
+IO.puts("A continuacion se ve reflejado el tipo de ganancia, \nque se obtendria con: 50000 de inversion con un 37% de interes\n")
 defmodule CalcPlazFij do
 
   def porcentaje(a,b) do
 
-    IO.puts "ganancia por dia: #{Float.round(((a * (b/100))/365),2)}"
-    IO.puts "ganancia por semana: #{Float.round((((a * (b/100))/365)*7),2)}"
-    IO.puts "ganancia por 30 dias: #{Float.round((((a * (b/100))/365)*30),2)}"
+    IO.puts "Ganancia por dia: #{Float.round(((a * (b/100))/365),2)}"
+    IO.puts "Ganancia por semana: #{Float.round((((a * (b/100))/365)*7),2)}"
+    IO.puts "Ganancia por 30 dias: #{Float.round((((a * (b/100))/365)*30),2)}"
+  end
+end
+IO.inspect(CalcPlazFij.porcentaje(50000,37))
+
+IO.puts("\nA continuacion vamos a sacar el porcentaje de ganancia de una inversion")
+IO.puts("Los datos proporcionados son ganancia de inversion: 3000, Cantidad de dias: 35, el monto de inversion es: 75000\n")
+defmodule CalcPorcInversion do
+
+  def porcInv(a,b,c) do
+
+    IO.puts "El porcentaje obtenido es: #{Float.round(((((a/b)*365)/c)*100),2)}%"
+
   end
 end
 
-IO.inspect(CalcPlazFij.porcentaje(50000,37))
+IO.inspect(CalcPorcInversion.porcInv(3000,35,75000))
+
+
+IO.puts("\nA continuacion vamos a sacar el monto de inversion requerido")
+IO.puts("Los datos proporcionados son ganancia ideal: 5000, Cantidad de dias: 60, Porcentaje: 30\n")
+defmodule CalcInversion do
+
+  def inv(a,b,c) do
+
+    IO.puts "La inversion requerida es: #{Float.round((((a/b)*365)/(c/100)),2)}"
+
+  end
+end
+
+IO.inspect(CalcInversion.inv(5000,60,30))
