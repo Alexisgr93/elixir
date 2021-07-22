8eb52256a18976f70071c3ad9bce06507170f854
Alexis Rodriguez <76778611+Alexisgr93@users.noreply.github.com>
2021-07-22 00:06:32 -0300

    Add files via upload

--- elixir/CalcPlazoFijo.exs
+++ elixir/CalcPlazoFijo.exs
@@ -7,12 +7,13 @@ defmodule Calculadora do
     #c = a + b
     #no hace falta que escriba return ni nada es solamen que poner c
     #c
-    a + b
-    cien = b / 100
-    dia = (a * cien)/365
 
+    IO.puts "ganancia por dia: #{Float.round(((a * (b/100))/365),2)}"
+    IO.puts "ganancia por semana: #{Float.round((((a * (b/100))/365)*7),2)}"
+    IO.puts "ganancia por 30 dias: #{Float.round((((a * (b/100))/365)*30),2)}"
   end
 end
 # IO.inspect(Calculadora.suma(2,3)) imprime el valor de una exprecion
 # IO.inspect sirve para imprimir cualquier cosa no como
 #IO.puts que sirve nada mas para imprimir cadenas de caracteres
+IO.inspect(Calculadora.suma(50000,37))
